4533a22acbd542410db769e0fe19565502b68c1c
Shoaib Ul Haq <shoaibulhaqq@outlook.com>
2024-07-08 19:45:04 +0500

    Updated random forest

--- app/app.py
+++ app/app.py
@@ -124,7 +124,7 @@ def show_accuracies():
     X_resampled, y_resampled = sm.fit_resample(X_scaled, y)
     
     # Split the resampled data
-    X_train, X_test, y_train, y_test = train_test_split(X_resampled, y_resampled, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X_resampled, y_resampled, test_size=0.3, random_state=42)
     
     accuracies = {}
     
